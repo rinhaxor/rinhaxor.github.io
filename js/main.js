2bb0754d8c9ab207985269a8b2def271c677366b
Rin Lê <rinoccho123@gmail.com>
2022-12-26 05:13:02 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
-function email(){
+$(document).ready(function(){
+	$("#trangchu-sothich").css("display", "block");
+	$("#nav-sothich").addClass("active");
+
+	$("#nav-sothich").click(function() {
+		clearActive();
+		$(this).addClass("active");
+		$("#trangchu-sothich").css("display", "block").addClass("active");
+		$("#trangchu-lienhe").css("display", "none");
+	});
+
+	$("#nav-lienhe").click(function() {
+		clearActive();
+		$(this).addClass("active");
+		$("#trangchu-sothich").css("display", "none");
+		$("#trangchu-lienhe").css("display", "block");
+	});
+});
+
+function clearActive() {
+	$("#nav-sothich").removeClass("active");
+	$("#nav-lienhe").removeClass("active");
+}
+
+function email() {
 	prompt("Gmail của tôi ở bên dưới", "rinle10032003@gmail.com");
+}
+function github(){
+	alert("Chưa thêm link Bé ơi");
 }
