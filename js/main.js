f51130a8c9ac5e294cebc336fa92be30d31956a6
Rin Lê <rinoccho123@gmail.com>
2022-12-29 16:57:47 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
 	// main
-	$("#trangchu-sothich").css("display", "block");
-	$("#nav-sothich").addClass("active");
+	$("#trangchu-mcs").css("display", "block");
+	$("#nav-mcs").addClass("active");
 
 	$("#nav-sothich").click(function() {
 		clearActive();
 		$(this).addClass("active");
-		$("#trangchu-sothich").css("display", "block").addClass("active");
+		$("#trangchu-sothich").css("display", "block");
+		$("#trangchu-lienhe").css("display", "none");
+		$("#trangchu-mcs").css("display", "none");
+	});
+
+	$("#nav-mcs").click(function() {
+		clearActive();
+		$(this).addClass("active");
+		$("#trangchu-mcs").css("display", "block").addClass("active");
+		$("#trangchu-sothich").css("display", "none");
 		$("#trangchu-lienhe").css("display", "none");
 	});
 
@@ -14,12 +23,14 @@ $(document).ready(function(){
 		clearActive();
 		$(this).addClass("active");
 		$("#trangchu-sothich").css("display", "none");
+		$("#trangchu-mcs").css("display", "none");
 		$("#trangchu-lienhe").css("display", "block");
 	});
 });
 
 function clearActive() {
 	$("#nav-sothich").removeClass("active");
+	$("#nav-mcs").removeClass("active");
 	$("#nav-lienhe").removeClass("active");
 }
 
